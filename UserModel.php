6258ae9514797e7b4596171fa78136ec77c523ff
akandeBolaji <akandebolajidanielofficial@gmail.com>
2021-04-14 00:08:43 +0100

    implement save api token on user model

--- UserModel.php
+++ UserModel.php
@@ -7,4 +7,14 @@ use akandebolaji\phpmvc\db\DbModel;
 abstract class UserModel extends DbModel
 {
     abstract public function getDisplayName(): string;
+
+    public function saveApiToken()
+    {
+        $tableName = $this->tableName();
+        print_r([$this->api_token, $this->api_token_expire_at]);
+        $statement = self::prepare("UPDATE $tableName SET api_token='$this->api_token', api_token_expire_at = '$this->api_token_expire_at' WHERE id = $this->id");
+
+        $statement->execute();
+        return true;
+    }
 }
