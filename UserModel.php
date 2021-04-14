c640ab5c9bf446be9a00a622e8ddf4fb6ee7cd2a
akandeBolaji <akandebolajidanielofficial@gmail.com>
2021-04-14 01:10:18 +0100

    add unroll api key to user model

--- UserModel.php
+++ UserModel.php
@@ -11,9 +11,15 @@ abstract class UserModel extends DbModel
     public function saveApiToken()
     {
         $tableName = $this->tableName();
-        print_r([$this->api_token, $this->api_token_expire_at]);
         $statement = self::prepare("UPDATE $tableName SET api_token='$this->api_token', api_token_expire_at = '$this->api_token_expire_at' WHERE id = $this->id");
+        $statement->execute();
+        return true;
+    }
 
+    public function unRollApiKey()
+    {
+        $tableName = $this->tableName();
+        $statement = self::prepare("UPDATE $tableName SET api_token=null, api_token_expire_at = null WHERE id = $this->id");
         $statement->execute();
         return true;
     }
